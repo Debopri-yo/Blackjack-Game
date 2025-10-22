54c1af318c10a2ffbfb5d183bf1a1a5d981d9960
Debopri-yo <ghoshlipika53@gmail.com>
2025-10-22 15:20:58 +0530

    fix:cards being unidentified objects,winloss logic.feat:standButton

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ let dealerCards=[];
 let psum=0,dsum=0;
 function drawCards(){
     let drawnCard;
-    const cardIndex = Math.floor(Math.random() * 52);
+    const cardIndex = Math.floor(Math.random() * cards.length);
     drawnCard= (cards[cardIndex]);
     cards.splice(cardIndex,1);
     return (drawnCard);
@@ -31,44 +31,48 @@ function beginGame(){
         psum=putValues(playerCards);
         if(psum>21){
             console.log('player lost');
-            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`);
+            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`, playerCards);
         }else if (psum>dsum){
-            console.log('player won');
-            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`);
+            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`, playerCards);
         }else if (psum===dsum){
-            console.log("it is a draw");
-            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`);
+            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`, playerCards);
         }else {
-            console.log("dealer is still ahead");
-            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`);
+            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`, playerCards);
         }
     });
     document.getElementById("standButton").addEventListener('click',function(){
-        if (dsum<psum){
+        if (dsum<psum && dsum<17){
             dealerCards.push(drawCards());
-            console.log(dsum += putValues(dealerCards));
-            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`);
+            dsum = putValues(dealerCards);
+            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`,dealerCards);
             if(dsum<=21 && dsum>psum){
             console.log('player lost');
-            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`);
+            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`,dealerCards);
             }else
             console.log('player won');
-            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`);
+            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`,dealerCards);
         }else{
             console.log('player lost');
-            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`);
+            console.log(`Player sum: ${psum}, Dealer sum: ${dsum}`,dealerCards);
         }
     });
 }
 beginGame();
 function putValues (array){
-let value=0;
+let value=0,aceCount=0;
     for (const element of array){
-        if (element.rank==="A"||element.rank==="K"||element.rank==="Q"||element.rank==="J"){
+        if (element.rank==="K"||element.rank==="Q"||element.rank==="J"){
             value+=10;
+        }else if(element.rank==="A"){
+            value+=11;
+            aceCount++;
         }else{
             value += Number(element.rank);
-        }
+        }    
+    }
+    while(value>21 && aceCount>0){
+        value -= 10;
+        aceCount--;
     }
     return value;
 }
